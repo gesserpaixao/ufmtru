b6ea32ce03ad34daed9ccfbba0270c7744d6263e
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2019-04-04 02:42:15 -0300

    Add files via upload

---
+++ laravel/index.php
@@ -0,0 +1,12 @@
+
+<?php
+header("Location: http://paw.ic.ufmt.br/ufmtru/public/index.php");
+?>
+<html>
+<title>Titulo do site</title>
+<head>
+<meta http-equiv="refresh" content=1;url="http://paw.ic.ufmt.br/ufmtru/public/index.php">
+</head>
+<body>
+</body>
+</html>
